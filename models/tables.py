9c388a978b22c4ee1d87f6bbbfe0f03cba14fb05
tylerhu3 <tylerhu@yahoo.com>
2018-05-05 20:17:46 -0700

    made some changes to tables.py and change some temp design changes to song upload page

--- models/tables.py
+++ models/tables.py
@@ -14,6 +14,7 @@ db.define_table('track',
                 Field('title'),
                 Field('artist', 'text'),
                 Field('popularity'),
+                Field('upvotes','integer'), #notice we are forcing age to be a int
                 Field('updated_on', 'datetime',
                       update=datetime.datetime.utcnow()),
                 )
@@ -22,11 +23,20 @@ db.track.title.writable = db.track.title.readable = False
 db.track.artist.writable = db.track.artist.readable = False
 db.track.popularity.writable = db.track.popularity.readable = False
 db.track.updated_on.writable = db.track.updated_on.readable = False
-
+db.track.upvotes.writable = False
+db.track.upvotes.readable = False
 db.define_table('spotify_user',
                 Field('username'),
                 Field('email'),
                 )
+
+
+db.define_table(
+    'artist', #we are creating a table call artist
+    Field('name'), # first field is call name, so thats why we use to grab data employees.name
+    Field('upvotes','integer') #notice we are forcing age to be a int
+    )
+
 
 db.spotify_user.username.writable = db.spotify_user.username.readable = False
 db.spotify_user.email.writable = db.spotify_user.email.readable = False
