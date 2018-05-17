3e295662a805dae0daaf860327fb7ee55095f0d3
Hasson Qayum <hqayum@ucsc.edu>
2018-05-17 15:59:44 -0700

    Profile Tabs progress

--- controllers/profile.py
+++ controllers/profile.py
@@ -1,2 +1,84 @@
+import spotipy
+from spotipy import oauth2
+import json
+import re
+
+# all sensitive info are stored in .env
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import os
+
+
 def index():
     return dict()
+
+def login(sp_oauth):
+    access_token = ""
+    token_info = sp_oauth.get_cached_token()
+
+    access_token = None
+    if token_info:
+        access_token = token_info['access_token']
+    else:
+        code = request.vars.code
+        if code:
+            token_info = sp_oauth.get_access_token(code)
+            access_token = token_info['access_token']
+    return access_token
+
+
+def getAuth():
+    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+    SCOPE = 'user-library-read'
+    CACHE = '.spotipyoauthcache'
+    sp_oauth = oauth2.SpotifyOAuth(
+        SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=SCOPE, cache_path=CACHE)
+    return sp_oauth
+
+def discoverData():
+    q = (db.spotify_user.username == request.vars.id)
+    spotify_user = db(q).select().first()
+    return response.json(spotify_user)
+
+
+def retrieveTotalLikes():
+    sp_oauth = getAuth()
+    access_token = login(sp_oauth)
+    if access_token:
+        sp = spotipy.Spotify(access_token)
+        results = sp.current_user()
+        q = (db.track.uploader == results["id"])
+        tracksUserUploaded = db(q).select()
+        return response.json(dict(tracksUserUploaded = tracksUserUploaded))
+    else:
+        pass
+
+def retrieveTotalLikesGiven():
+    sp_oauth = getAuth()
+    access_token = login(sp_oauth)
+    if access_token:
+        sp = spotipy.Spotify(access_token)
+        results = sp.current_user()
+        q = (db.spotify_user.username == results["id"])
+        spotify_user = db(q).select().first()
+        q2 = (db.upvotes.upvoter == spotify_user.username)
+        retrieveTotalLikesGiven = db(q2).select()
+        return response.json(dict(retrieveTotalLikesGiven = retrieveTotalLikesGiven))
+    else:
+        pass
+
+def retrieveTotalLikesReceived():
+    sp_oauth = getAuth()
+    access_token = login(sp_oauth)
+    if access_token:
+        sp = spotipy.Spotify(access_token)
+        results = sp.current_user()
+        q = (db.spotify_user.username == results["id"])
+        spotify_user = db(q).select().first()
+        q2 = (db.upvotes.uploaderOfSong == spotify_user.username)
+        retrieveTotalLikesReceived = db(q2).select()
+        return response.json(dict(retrieveTotalLikesReceived = retrieveTotalLikesReceived))
+    else:
+        pass
---
+++ static/js/default_profile.js
@@ -0,0 +1,78 @@
+var app = function() {
+    var url = "{{=URL('default', 'index')}}";
+    var self = {};
+    var musicArray = [];
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    function findUsername(id) {
+      var email = null;
+      $.post(discoverData, {
+        id:id
+      }, function(data) {
+          console.log(data);
+      })
+        return email;
+    }
+
+    self.userTotalLikes = function() {
+      var sum = 0;
+      $.post(totLikes, {}, function(data){
+        self.vue.tracksUserUploaded = data.tracksUserUploaded;
+        for(var i = 0;i<data.tracksUserUploaded.length;i++) {
+          sum += data.tracksUserUploaded[i].upvotes;
+        }
+        console.log(data.tracksUserUploaded);
+      })
+    }
+
+    self.songsUserLikes = function() {
+      $.post(givenLikes, {}, function(data){
+            self.vue.countLikesOfUserUploaded = data.retrieveTotalLikesGiven;
+          console.log(self.vue.countLikesOfUserUploaded);
+        })
+      }
+
+      self.songsLikesReceived = function() {
+        $.post(receivedLikes, {}, function(data){
+              for(var i = 0;i<data.retrieveTotalLikesReceived.length;i++) {
+                if(data.retrieveTotalLikesReceived[i].upvoter === null)
+                  data.retrieveTotalLikesReceived[i].upvoter = "Anonymous User";
+              }
+              self.vue.ReceivedUserLikes =  data.retrieveTotalLikesReceived;
+          })
+        }
+
+    // Complete as needed.
+    self.vue = new Vue({
+        el: "#parent",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            tracksUserUploaded: Array,
+            countLikesOfUserUploaded: Array,
+            ReceivedUserLikes: Array
+        },
+        methods: {
+          userTotalLikes: self.userTotalLikes,
+    },
+
+    });
+    self.userTotalLikes();
+    self.songsUserLikes();
+    self.songsLikesReceived();
+    $("#parent").show();
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
